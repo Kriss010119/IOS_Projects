b0959c446763e6fff536d0f412e271167ba9d9e3
Kriss010119 <dardardar717@gmail.com>
2025-09-15 22:05:33 +0300

    Initial Commit

---
+++ deosinaPW1Tests/deosinaPW1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  deosinaPW1Tests.swift
+//  deosinaPW1Tests
+//
+//  Created by Kriss Osina on 13.09.2025.
+//
+
+import Testing
+@testable import deosinaPW1
+
+struct deosinaPW1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
